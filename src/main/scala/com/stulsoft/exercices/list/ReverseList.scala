259107e3f9a50f23dc10f6c293c13c0700ddcf76
ysden123 <ysden123@gmail.com>
2017-08-11 20:36:37 +0300

    recursion and foldLeft

--- src/main/scala/com/stulsoft/exercices/list/ReverseList.scala
+++ src/main/scala/com/stulsoft/exercices/list/ReverseList.scala
@@ -13,6 +13,8 @@ import scala.collection.immutable.VectorBuilder
 object ReverseList extends App {
   println("==>ReverseList.main")
   test(List(1, 2, 3, 4))
+  test(List())
+  test(List("a"))
   println("<==ReverseList.main")
 
   /**
@@ -22,7 +24,9 @@ object ReverseList extends App {
     * @tparam T type of list elements
     */
   def test[T](list: List[T]): Unit = {
-    println(s"Reverese of [$list] id [${reverseList(list)}]")
+    println(s"(1) Reverse of [$list] id [${reverseList1(list)}]")
+    println(s"(2) Reverse of [$list] id [${reverseList2(list)}]")
+    println(s"(3) Reverse of [$list] id [${reverseList3(list)}]")
   }
 
   /**
@@ -32,10 +36,37 @@ object ReverseList extends App {
     * @tparam T type of list elements
     * @return the reversed list
     */
-  def reverseList[T](list: List[T]): List[T] = {
+  def reverseList1[T](list: List[T]): List[T] = {
     val result = new VectorBuilder[T]
     for (i <- list.indices)
       result += list(list.length - 1 - i)
     result.result.toList
+  }
+
+  /**
+    * Reverses a list using recursion
+    *
+    * @param list the list
+    * @tparam T type of list elements
+    * @return the reversed list
+    */
+  def reverseList2[T](list: List[T]): List[T] = {
+    def reverseList(result: List[T], currentList: List[T]): List[T] = currentList match {
+      case Nil => result
+      case h :: tail => reverseList(h :: result, tail)
+    }
+
+    reverseList(Nil, list)
+  }
+
+  /**
+    * Reverses a list using foldLeft
+    *
+    * @param list the list
+    * @tparam T type of list elements
+    * @return the reversed list
+    */
+  def reverseList3[T](list: List[T]): List[T] = {
+    list.foldLeft(List[T]()) { (result, head) => head :: result }
   }
 }
