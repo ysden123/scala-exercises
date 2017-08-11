91eac4b7158a6372c38226ab4a617f648293e44a
ysden123 <ysden123@gmail.com>
2017-08-11 08:58:55 +0300

    Find last element of collection

--- src/main/scala/com/stulsoft/exercices/list/FindLastElement.scala
+++ src/main/scala/com/stulsoft/exercices/list/FindLastElement.scala
@@ -2,20 +2,34 @@ package com.stulsoft.exercices.list
 
 import scala.annotation.tailrec
 
-/**
+/** Finds the last element of a collection
+  *
   * @author Yuriy Stul.
   */
 object FindLastElement extends App {
   println("==>FindLastElement.main")
   test1(Seq(1, 2, 3))
-  test1(Seq("abcd"))
-  test1(Seq("abcd".toList))
+  test1("abcd".toList)
+  test1(Nil)
   println("<==FindLastElement.main")
 
+  /**
+    * Test runner
+    *
+    * @param collection collection to handle
+    * @tparam T specifies the type of collection elements
+    */
   def test1[T](collection: Seq[T]): Unit = {
     println(s"""Last element of [$collection] is ${getLastElement(collection)} """)
   }
 
+  /**
+    * Returns a last element of a collection
+    *
+    * @param collection collection
+    * @tparam T specifies the type of collection elements
+    * @return the last element of a collection
+    */
   @tailrec
   def getLastElement[T](collection: Seq[T]): Option[T] = {
     if (collection == Nil)
