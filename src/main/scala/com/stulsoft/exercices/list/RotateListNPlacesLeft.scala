016524047f523999b43ff26eb5f7a89633ac68bc
Yuriy Stul <Yuriy.Stul@WilliamHill.com>
2017-08-15 13:05:17 +0300

    rotatte (not completed)

--- src/main/scala/com/stulsoft/exercices/list/RotateListNPlacesLeft.scala
+++ src/main/scala/com/stulsoft/exercices/list/RotateListNPlacesLeft.scala
@@ -12,6 +12,22 @@ package com.stulsoft.exercices.list
   * @author Yuriy Stul.
   */
 object RotateListNPlacesLeft extends App {
+  test(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
 
-  def rotate1[T](n: Int, list: List[T]): List[T] = ???
+  def test[T](n: Int, list: List[T]): Unit = {
+    println(s"(1) $list with n=$n -> ${rotate1(n, list)}")
+  }
+
+  def rotate1[T](n: Int, list: List[T]): List[T] = {
+    def rotate(curN: Int, curR: List[T], curSuffix:List[T], next: List[T]): List[T] = (curN, next) match {
+      case (_, Nil) =>
+        curR ++ curSuffix
+      case (0, ls) =>
+        curR ++ ls
+      case (cn, h :: t) =>
+        rotate(cn - 1, curR :+ h, Nill, t)
+    }
+
+    rotate(n, Nil, Nill, list)
+  }
 }
