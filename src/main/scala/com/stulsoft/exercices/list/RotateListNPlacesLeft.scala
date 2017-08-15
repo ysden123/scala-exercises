e03bb7bb774eaea4f299f2b7eda6c75242ce74b5
ysden123 <ysden123@gmail.com>
2017-08-15 21:23:53 +0300

    RotateListNPlacesLeft

--- src/main/scala/com/stulsoft/exercices/list/RotateListNPlacesLeft.scala
+++ src/main/scala/com/stulsoft/exercices/list/RotateListNPlacesLeft.scala
@@ -1,6 +1,9 @@
 package com.stulsoft.exercices.list
 
-/** Rotate a list N places to the left
+import scala.annotation.tailrec
+
+/**
+  * Rotate a list N places to the left
   * <p>
   * Examples:
   * {{{rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))}}}
@@ -13,21 +16,48 @@ package com.stulsoft.exercices.list
   */
 object RotateListNPlacesLeft extends App {
   test(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  test(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  test(9, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
 
+  /**
+    * Test runner
+    *
+    * @param n    where to rotate
+    * @param list the list
+    * @tparam T list items type
+    */
   def test[T](n: Int, list: List[T]): Unit = {
     println(s"(1) $list with n=$n -> ${rotate1(n, list)}")
   }
 
+  /**
+    * Rotates
+    *
+    * @param n    where to rotate
+    * @param list the list
+    * @tparam T list items type
+    * @return rotated list
+    */
   def rotate1[T](n: Int, list: List[T]): List[T] = {
-    def rotate(curN: Int, curR: List[T], curSuffix:List[T], next: List[T]): List[T] = (curN, next) match {
-      case (_, Nil) =>
-        curR ++ curSuffix
-      case (0, ls) =>
-        curR ++ ls
-      case (cn, h :: t) =>
-        rotate(cn - 1, curR :+ h, Nill, t)
+
+    /**
+      * Rotates recursively
+      *
+      * @param skip   how to skip from the beginning
+      * @param suffix elements from the beginning, will be added at the end
+      * @param rest   the rest of the source list
+      * @return rotated list
+      */
+    @tailrec
+    def rotate(skip: Int, suffix: List[T], rest: List[T]): List[T] = (skip, rest) match {
+      case (_, Nil) => rest ::: suffix
+      case (0, _) => rest ::: suffix
+      case (_, _) => rotate(skip - 1, suffix :+ rest.head, rest.tail)
     }
 
-    rotate(n, Nil, Nill, list)
+    if (n > 0)
+      rotate(n, Nil, list)
+    else
+      rotate(list.length + n, Nil, list)
   }
 }
