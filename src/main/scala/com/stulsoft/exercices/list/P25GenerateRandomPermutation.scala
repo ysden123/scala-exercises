0986e1a71f433af32ddd014dc5435cb1c197484b
Yuriy Stul <Yuriy.Stul@WilliamHill.com>
2017-08-17 14:34:04 +0300

    P25GenerateRandomPermutation

---
+++ src/main/scala/com/stulsoft/exercices/list/P25GenerateRandomPermutation.scala
@@ -0,0 +1,32 @@
+package com.stulsoft.exercices.list
+
+/**
+  * Generate a random permutation of the elements of a list
+  *
+  * @author Yuriy Stul.
+  */
+object P25GenerateRandomPermutation extends App {
+
+  test(List(1, 2, 3, 4, 5, 6))
+
+  /**
+    * Test runner
+    *
+    * @param ls the list
+    * @tparam T list elements type
+    */
+  def test[T](ls: List[T]) {
+    println(s"(1) $ls -> ${generate1(ls)}")
+  }
+
+  /**
+    * Generate a random permutation of the elements of a list
+    *
+    * @param ls the list
+    * @tparam T list elements type
+    * @return result
+    */
+  def generate1[T](ls: List[T]): List[T] = {
+    P23ExtractGivenRandomElements.randomSelect1(ls.length, ls)
+  }
+}
