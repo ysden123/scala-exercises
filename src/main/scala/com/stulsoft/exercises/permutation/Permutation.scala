5d62cd1b5276f736b028e5f068791b21b40d118b
ysden123 <ysden123@gmail.com>
2017-08-19 21:26:46 +0300

    permutations2 (same)

--- src/main/scala/com/stulsoft/exercises/permutation/Permutation.scala
+++ src/main/scala/com/stulsoft/exercises/permutation/Permutation.scala
@@ -5,23 +5,28 @@
 package com.stulsoft.exercises.permutation
 
 /**
-  * @see [[https://gist.github.com/AhmedSoliman/ce17e5dd4927f4349696 Generate Permutations of String in Scala]]
+  * @see [[https://www.youtube.com/watch?v=_1YHHlX1zvc Permutations (using Scala]]
   * @author Yuriy Stul
   */
 object Permutation extends App {
 
   test(List(1, 2, 3))
+  //  test(List(1, 2, 3, 4))
 
   def test(input: List[Int]): Unit = {
-    println(s"(1) input: $input, permutations:")
-    permutations1(input).foreach(x => println(s"   $x"))
+    var permutations = permutations1(input)
+    println(s"(1) input: $input, (${permutations.length}) permutations:")
+    permutations.foreach(x => println(s"   $x"))
+
+    permutations = permutations2(input)
+    println(s"(2) input: $input, (${permutations.length}) permutations:")
+    permutations.foreach(x => println(s"   $x"))
   }
 
   def permutations1(list: List[Int]): List[List[Int]] = list match {
-    case Nil => List(Nil)
-    case List(_) => List(list)
-    case x =>
-      println(x)
+    case Nil => List(Nil) // Empty input list
+    case List(_) => List(list) // Input is list with only one element
+    case _ => // Input is list with more than one elements
       (
         for (i <- list.indices.toList) yield {
           val (before, rest) = list.splitAt(i)
@@ -29,5 +34,22 @@ object Permutation extends App {
           val subPermutations = permutations1(before ++ rest.tail)
           subPermutations.map(element :: _)
         }).flatten
+  }
+
+  def permutations2(list: List[Int]): List[List[Int]] = {
+    def permutations(currentList: List[Int]): List[List[Int]] = currentList match {
+      case Nil => List(Nil) // Empty input list
+      case List(_) => List(currentList) // Input is list with only one element
+      case x => // Input is list with more than one elements
+        (
+          for (i <- x.indices.toList) yield {
+            val (before, rest) = x.splitAt(i)
+            val element = rest.head
+            val subPermutations = permutations(before ++ rest.tail)
+            subPermutations.map(element :: _)
+          }).flatten
+    }
+
+    permutations(list)
   }
 }
