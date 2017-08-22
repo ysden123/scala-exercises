a95b89ff376890e65f167647b23a59bf0804f07c
Yuriy Stul <Yuriy.Stul@WilliamHill.com>
2017-08-22 15:50:20 +0300

    P31DetermineWhetherPrime

---
+++ src/main/scala/com/stulsoft/exercises/arithmetic/P31DetermineWhetherPrime.scala
@@ -0,0 +1,39 @@
+package com.stulsoft.exercises.arithmetic
+
+/** Determine whether a given integer number is prime.
+  * <p>
+  * Example:
+  * {{{7.isPrime}}}
+  * {{{true}}}
+  *
+  * @author Yuriy Stul.
+  */
+object P31DetermineWhetherPrime extends App {
+
+  test(1)
+  test(2)
+  test(3)
+  test(4)
+  test(5)
+  test(6)
+
+  /** Test runner
+    */
+  def test(n: Int): Unit = {
+    println(s"(1) $n is prime: ${isPrime1(n)}")
+  }
+
+  /** Checks whether n is prime or not
+    *
+    * @param n the n
+    * @return true, if n is prime; false otherwise
+    */
+  def isPrime1(n: Int): Boolean = {
+    if (n <= 1)
+      false
+    else if (n == 2)
+      true
+    else
+      !(2 until n).exists(x => n % x == 0)
+  }
+}
