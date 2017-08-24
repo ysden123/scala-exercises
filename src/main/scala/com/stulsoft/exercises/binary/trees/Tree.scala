45bdd5df2fe6aaf1722d359b19985ffe83c72d20
Yuriy Stul <Yuriy.Stul@WilliamHill.com>
2017-08-24 17:16:08 +0300

    P56SymmetricBinaryTrees

--- src/main/scala/com/stulsoft/exercises/binary/trees/Tree.scala
+++ src/main/scala/com/stulsoft/exercises/binary/trees/Tree.scala
@@ -3,14 +3,31 @@ package com.stulsoft.exercises.binary.trees
 /**
   * @author Yuriy Stul.
   */
-trait Tree[+T]
+trait Tree[+T] {
+  def isSymmetric: Boolean
+
+  def isMirrorOf[V](t: Tree[V]): Boolean
+}
 
 case object End extends Tree[Nothing] {
   override def toString: String = "."
+
+  override def isSymmetric = true
+
+  override def isMirrorOf[V](t: Tree[V]): Boolean = t == End.this
 }
 
 case class Node[T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
   override def toString: String = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
+
+
+  override def isSymmetric: Boolean = left.isMirrorOf(right)
+
+
+  override def isMirrorOf[V](tree: Tree[V]): Boolean = tree match {
+    case t: Node[T] => left.isMirrorOf(t.right) && right.isMirrorOf(t.left)
+    case _ => false
+  }
 
   def addNodeToLeft(newNode: Node[T]): Node[T] = {
     Node(value, newNode, this.right)
